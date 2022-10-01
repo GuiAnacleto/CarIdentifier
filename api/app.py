4beead1dedb2af469fefdb7fa210a6c9290d38af
Elias Santana <eliasvssantana@gmail.com>
2022-10-01 14:51:18 -0300

    adjustment api endpoint

--- api/app.py
+++ api/app.py
@@ -1,1 +1,19 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec
+from datetime import datetime
+from pytz import timezone
+
+server = Flask(__name__)
+spec = FlaskPydanticSpec('flask', title='CarIdentifier')
+
+spec.register(server)
+
+@server.get('/camera')
+def get_camera():
+    hora_atual = datetime.now()
+    fuso_horario = timezone('America/Sao_Paulo')
+
+    hora = hora_atual.astimezone(fuso_horario)
+    return hora.strftime("%A-%m-%Y %H:%M:%S")
+
+server.run()
