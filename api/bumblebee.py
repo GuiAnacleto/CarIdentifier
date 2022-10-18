46eb1a358a83ec0e5a20909e4eff018eadbf07ba
Elias Santana <eliasvssantana@gmail.com>
2022-10-18 00:22:51 -0300

    add funções de captura video e imagem

--- api/bumblebee.py
+++ api/bumblebee.py
@@ -1,16 +1,36 @@
 import cv2 as cv
 
-camera = cv.VideoCapture("")
+def captura_video():
+    '''Função que captura a live da camera para a analise'''
+    camera = cv.VideoCapture("")
 
-if camera.isOpened():
-    conectado, frame = camera.read()
-
-    while conectado:
+    if camera.isOpened():
         conectado, frame = camera.read()
-        cv.imshow(f"Camera {id}", frame) #Abre uma janela com a imagem coletada
-        key = cv.waitKey(3)
-        if key == 27:
-            break
 
-camera.release()
-cv.destroyAllWindows()
+        while conectado:
+            conectado, frame = camera.read()
+            cv.imshow(f"Camera {id}", frame) #Abre uma janela com a imagem coletada
+            key = cv.waitKey(3)
+            if key == 27:
+                break
+    #cv.imwrite("deagora.png", frame) #Teste do frame capturado
+    camera.release()
+    cv.destroyAllWindows()
+
+
+def captura_frame(id):
+    '''Função que captura o frame para a analise'''
+
+    if id == 0:
+        camera = cv.VideoCapture("")
+    elif id == 1:
+        camera = cv.VideoCapture("")
+    else:
+        frame = cv.imread('imagem-com-carro.jpg')
+        return frame
+
+    frame = camera.read()
+    camera.release()
+    cv.destroyAllWindows()
+
+    return frame[1]
